87d6b425d49f5405cdabcd479d8d600344733a33
Fengalex0 <3153366821@qq.com>
2023-01-06 12:53:13 +0800

    前端(完成) , 后端(解决搜索结果重复问题; 待办:日志系统)

---
+++ boost_search_engine/index.hpp
@@ -0,0 +1,191 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <fstream>
+#include <mutex>
+
+#include "Util.hpp"
+
+namespace NS_index
+{
+
+    struct DocInfo
+    {
+        std::string title;
+        std::string content;
+        std::string url;
+        uint64_t doc_id; // 文档id
+    };
+
+    struct InvertedElem
+    {
+        uint64_t doc_id;
+        std::string word;
+        int weight;
+    };
+
+    typedef std::vector<InvertedElem> InvertedList; // 倒排拉链
+
+    class Index
+    {
+    private:
+        // 正排索引的数据结构用数组，数组的下标天然是文档的ID
+        std::vector<DocInfo> forward_index;
+
+        // 倒排索引一定是一个关键字和一组(个)InvertedElem对应[关键字和倒排拉链的映射关系]
+        std::unordered_map<std::string, InvertedList> inverted_index;
+
+    private:
+        Index(){};
+        Index(const Index &) = delete;
+        Index &operator=(const Index &) = delete;
+
+        static Index *instance;
+        static std::mutex mtx;
+
+    public:
+        ~Index(){};
+
+        static Index *GetInstance()
+        {
+            if (nullptr == instance)
+            {
+                std::unique_lock<std::mutex> lk(mtx);
+                if (nullptr == instance)
+                {
+                    instance = new Index();
+                }
+            }
+
+            return instance;
+        }
+
+        // 根据doc_id找到找到文档内容
+        DocInfo *GetForwardIndex(uint64_t doc_id)
+        {
+            if (doc_id >= forward_index.size())
+            {
+                std::cerr << "doc_id out range ,error" << std::endl;
+                return nullptr;
+            }
+            return &forward_index[doc_id];
+        }
+
+        // 根据关键字string，获得倒排拉链
+        InvertedList *GetInveredList(const std::string &word)
+        {
+            auto iter = inverted_index.find(word);
+            if (iter == inverted_index.end())
+            {
+                std::cerr << word << "have no InvertedList" << std::endl;
+                return nullptr;
+            }
+            return &(iter->second);
+        }
+
+        // 根据去标签，格式化之后的文档，构建正排和倒排索引
+        bool BuildIndex(const std::string &input)
+        {
+            std::ifstream in(input, std::ios::in | std::ios::binary);
+            if (!in.is_open())
+            {
+                std::cerr << "sorry ," << input << "open error " << std::endl;
+                return false;
+            }
+
+            std::string line;
+            int count = 0;
+            while (std::getline(in, line))
+            {
+                DocInfo *doc = BulidForwardIndex(line);
+                if (nullptr == doc)
+                {
+                    std::cerr << "build" << line << "error" << std::endl;
+                    continue;
+                }
+
+                BuildInvertedIndex(*doc);
+            }
+            return true;
+        }
+
+    private:
+        DocInfo *BulidForwardIndex(const std::string &line)
+        {
+            // 解析line，字符串切分
+            std::vector<std::string> results;
+            const std::string sep = "\3";
+            NS_util::StringUtil::Split(line, &results, sep);
+            if (results.size() != 3)
+            {
+                return nullptr;
+            }
+
+            // 字符串进行填充到DocIinfo
+            DocInfo doc;
+            doc.title = results[0];
+            doc.content = results[1];
+            doc.url = results[2];
+            doc.doc_id = forward_index.size();
+            forward_index.push_back(std::move(doc)); // 先进行保存id，在插入，对应的id就是当前doc在vector中的下标!
+
+            // 插入到正排索引的vector
+            return &forward_index.back();
+        }
+
+        bool BuildInvertedIndex(const DocInfo &doc)
+        {
+            // DocInfo{title, content, url, doc_id}
+            struct word_cnt
+            {
+                int title_cnt;
+                int content_cnt;
+
+                word_cnt() : title_cnt(0), content_cnt(0){};
+            };
+
+            std::unordered_map<std::string, word_cnt> word_map; // 暂存词频的映射表
+
+            // 对标题进行分词
+            std::vector<std::string> title_words;
+            NS_util::JiebaUtil::CutString(doc.title, &title_words);
+
+            // 对标题进行词频统计
+            for (std::string s : title_words)
+            {
+                boost::to_lower(s); ////需要统一转化成为小写,因为一般搜索引擎不区分大小写
+                word_map[s].title_cnt++;
+            }
+
+            // 对文档内容进行分词
+            std::vector<std::string> content_words;
+            NS_util::JiebaUtil::CutString(doc.content, &content_words);
+
+            // 对内容进行词频统计
+            for (std::string s : content_words)
+            {
+                boost::to_lower(s);
+                word_map[s].content_cnt++;
+            }
+
+#define X 10
+#define Y 1
+            for (auto &word_pair : word_map)
+            {
+                InvertedElem item;
+                item.doc_id = doc.doc_id;
+                item.weight = word_pair.second.title_cnt * X + word_pair.second.content_cnt * Y;
+                item.word = word_pair.first;
+                InvertedList &inverted_list = inverted_index[word_pair.first];
+                inverted_list.push_back(std::move(item));
+            }
+
+            return true;
+        }
+    };
+    Index *Index::instance = nullptr;
+    std::mutex Index::mtx;
+}
---
+++ boost_search_engine/searcher.hpp
@@ -0,0 +1,128 @@
+#pragma once
+
+#include "index.hpp"
+#include <jsoncpp/json/json.h>
+
+namespace NS_search
+{
+
+    struct InvertedElemPrint
+    {
+        uint64_t doc_id;
+        int weight;
+        std::vector<std::string> words;
+
+        InvertedElemPrint() : doc_id(0), weight(0) {}
+    };
+
+    class Searcher
+    {
+    private:
+        NS_index::Index *index;
+
+    public:
+        Searcher(){};
+        ~Searcher(){};
+
+        void InitSearcher(const std::string &input)
+        {
+            // 获取或者创建index对象
+            index = NS_index::Index::GetInstance();
+
+            index->BuildIndex(input);
+        }
+
+        void Search(const std::string &query, std::string *json_string)
+        {
+            // 分词:对我们的query进行按照searcher的要求进行分词
+            std::vector<std::string> words;
+            NS_util::JiebaUtil::CutString(query, &words);
+
+            // 触发:就是根据分词的各个"词"，进行ind ex查找,建立index是忽略大小写，所以搜索，关键字也需要
+            std::vector<InvertedElemPrint> inverted_list_all;
+            std::unordered_map<uint64_t, InvertedElemPrint> tokens_map;
+            for (auto word : words)
+            {
+                boost::to_lower(word);
+
+                NS_index::InvertedList *inverted_list = index->GetInveredList(word);
+                if (nullptr == inverted_list)
+                {
+                    continue;
+                }
+
+                for (const auto &elem : *inverted_list)
+                {
+                    // item一定是doc_id相同的print节点
+                    auto &item = tokens_map[elem.doc_id];
+                    item.doc_id = elem.doc_id;
+                    item.weight += elem.weight;
+                    item.words.push_back(elem.word);
+                }
+            }
+            for (const auto &item : tokens_map)
+            {
+                inverted_list_all.push_back(std::move(item.second));
+            }
+
+            // 合并排序：汇总查找结果，按照相关性(weight)降序排序
+            std::sort(
+                inverted_list_all.begin(), inverted_list_all.end(), [](const InvertedElemPrint &e1, const InvertedElemPrint &e2)
+                { return e1.weight > e2.weight; });
+
+            Json::Value root;
+            for (auto &item : inverted_list_all)
+            {
+                NS_index::DocInfo *doc = index->GetForwardIndex(item.doc_id);
+                if (nullptr == doc)
+                {
+                    continue;
+                }
+                Json::Value elem;
+                elem["title"] = doc->title;
+                elem["desc"] = GetDesc(doc->content, item.words[0]);
+                elem["url"] = doc->url;
+                elem["id"] = (int)item.doc_id;
+                elem["weight"] = item.weight;
+
+                root.append(elem);
+            }
+
+            // Json::FastWriter writer;
+            Json::StyledWriter writer;
+            *json_string = writer.write(root);
+        }
+
+        // 找到word在html_content中的首次出现，然后往前找50字节(如果没有，从begin开始)，往后找100字节(如果没有，到end就可以的)
+        // 截取出这部分内容
+        std::string GetDesc(const std::string &html_content, const std::string word)
+        {
+            const int prev_step = 50;
+            const int next_step = 50;
+            // 1. 找到首次出现的关键词，为什么不使用find，因为find是区分大小写的
+            auto iter = std::search(html_content.begin(), html_content.end(), word.begin(), word.end(), [](int x, int y)
+                                    { return (std::tolower(x) == std::tolower(y)); });
+            if (iter == html_content.end())
+            {
+                return "None!";
+            }
+
+            int pos = std::distance(html_content.begin(), iter);
+
+            int start = 0;
+            int end = html_content.size() - 1;
+
+            // 如果之前有50+字符，就更新开始位置
+            if (pos > start + prev_step)
+                start = pos - prev_step;
+            if (pos < end - next_step)
+                end = pos + next_step;
+
+            if (start >= end)
+                return "None2";
+            std::string desc = html_content.substr(start, end - start);
+            desc += "...";
+            return desc;
+        }
+    };
+}
